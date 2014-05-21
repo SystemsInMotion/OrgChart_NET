9d03ee00c37b6c2329570aae8824484e26feb7e3
apavlov <apavlov@systemsinmotion.com>
2014-05-21 15:29:32 -0400

    OCN-12, home page update

--- Orgchart.Web/Models/Employee.cs
+++ Orgchart.Web/Models/Employee.cs
@@ -1,22 +1,52 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
 namespace Orgchart.Web.Models
 {
+    [Table("Employee")]
     public class Employee
     {
-        [DatabaseGenerated(DatabaseGeneratedOption.None)]
-        public int EmployeeId { get; set; }
-        public string first_name { get; set; }
-        public string last_name { get; set; }
-        public string email { get; set; }
-        public string skype_name { get; set; }
-        public int job_title_id { get; set; }
-        public bool is_manager { get; set; }
-        public int manager_id { get; set; }
-        public int department_id { get; set; }
+
+        [Key]
+        [Column("Employee_ID")]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(45)]
+        [Column("first_name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(45)]
+        [Column("last_name")]
+        public string LastName { get; set; }
+
+        [Required]
+        [StringLength(45)]
+        [Column("email")]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(45)]
+        [Column("skype_name")]
+        public string SkypeName { get; set; }
+
+        [Required]
+        [Column("job_title_id")]
+        public int JobTitleId { get; set; }
+
+        [Required]
+        [Column("is_manager")]
+        public bool IsManager { get; set; }
+
+        [Column("manager_id")]
+        public int ManagerId { get; set; }
+
+        [Column("department_id")]
+        public int DepartmentId { get; set; }
     }
 }
